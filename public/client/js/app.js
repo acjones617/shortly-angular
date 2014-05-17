2ef619a8cef02ae341cd98ec59e3bccbe4076e81
Hack Reactor Students <acjones617@gmail.com>
2014-05-17 00:16:58 -0700

    Adds functionality to mess around with all links page

--- public/client/js/app.js
+++ public/client/js/app.js
@@ -15,15 +15,22 @@ app.config(['$routeProvider', function($routeProvider) {
   .otherwise({redirectTo: '/trerereer'});
 }]);
 
+
+
 app.controller('linksController', function($scope, Links) {
   $scope.links = [];
 
+  $scope.search = '';
+
   Links.getLinks().then(function(links) {
+    $scope.predicate = '-visits';
     $scope.links = links.data;
+    console.log(links.data);
   });
 });
 
 app.controller('createController', function($scope, Links) {
+  $scope.pattern = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i
   $scope.shorten = {};
   $scope.spinner = false;
   $scope.newLinks = false;
