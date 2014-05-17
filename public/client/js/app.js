d5bded804284b1d47acd1a35056181b51e7ccd35
Hack Reactor Students <acjones617@gmail.com>
2014-05-16 23:42:36 -0700

    Adds angular router

--- public/client/js/app.js
+++ public/client/js/app.js
@@ -1,26 +1,62 @@
-var app = angular.module('shortly', []);
+var app = angular.module('shortly', ['ngRoute']);
 
-app.controller('linksController', function($scope, getLinks) {
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    controller: 'linksController',
+    // template: '<h1>HOME</h1>'
+    templateUrl: 'client/templates/home.html'
+  })
+  .when('/create', {
+    controller: 'createController',
+    // template: '<h1>CREATE</h1>'
+    templateUrl: 'client/templates/shorten.html'
+  })
+  .otherwise({redirectTo: '/trerereer'});
+}]);
+
+app.controller('linksController', function($scope, Links) {
   $scope.links = [];
 
-  getLinks.getLinks().then(function(links) {
-    console.log(links, 'in controller');
+  Links.getLinks().then(function(links) {
     $scope.links = links.data;
   });
 });
 
-// app.controller('createController', function($scope) {
-//   $scope.create;
-// });
+app.controller('createController', function($scope, Links) {
+  $scope.shorten = {};
+  $scope.spinner = false;
+  $scope.newLinks = false;
+  $scope.shorten.submit = function() {
+    $scope.failMessage = '';
+    $scope.spinner = true;
+    Links.postLinks($scope.shorten.url)
+    .success(function(link) {
+      console.log(link);
+      $scope.spinner = false;
+      $scope.newLinks = true;
+      $scope.link = link;
+    })
+    .error(function() {
+      console.log('gets to failure');
+      $scope.failMessage = 'Not a valid URL, try again';
+      $scope.spinner = false;
+    });
+  };
+});
 
-app.factory('getLinks', function($http) {
-
+app.factory('Links', function($http) {
   return {
     getLinks: function() {
       return $http({
         method: 'GET',
         url: '/links'
       });
+    },
+
+    postLinks: function(link) {
+      console.log('posting link');
+      return $http.post('/links', {url: link});
     }
   };
 });
